d8228f2df97e665e5a5ccfd1c9ba1c6d8a085f33
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-07-04 22:23:55 +0530

    you can create posts in newsgroup mode now !!!!!

--- src/routes/[address]/news/+page.server.js
+++ src/routes/[address]/news/+page.server.js
@@ -1,5 +1,8 @@
 import {db, newsgroupAddress} from "$lib/server/db/index.js";
-import {asc, desc, eq} from "drizzle-orm";
+import {asc, eq} from "drizzle-orm";
+import * as auth from "$lib/server/auth.js";
+import {invalidateGuestSession, invalidateSession, validateSessionToken} from "$lib/server/auth.js";
+import {fail} from "@sveltejs/kit";
 
 export const load = async ({ params, depends }) => {
     depends(`news:${params.address}`);
@@ -14,3 +17,53 @@ export const load = async ({ params, depends }) => {
             .limit(100)
     };
 };
+
+export const actions = {
+    createPost: async (event) => {
+        if (!event.locals.session) return fail(401, { message: 'Unauthorized' });
+        if (!event.locals.user) return fail(401, { message: 'Unauthorized' });
+
+        if (!event.locals.user.isGuest) {
+            const session = await validateSessionToken(event.cookies.get(auth.sessionCookieName));
+            if (session.session.id !== event.locals.session.id) {
+                try { await invalidateSession(event.locals.session.id); } catch {}
+                try { await invalidateGuestSession(event.locals.session.id); } catch {}
+                return fail(403, { message: "Invalid session token. Reload and log back in." });
+            }
+        }
+
+        const formData = await event.request.formData();
+        const image = formData.get('image');
+        let imageUpload;
+
+        if (!(image instanceof File) || image.size === 0) {}
+        else {
+            const buffer = Buffer.from(await image.arrayBuffer());
+            const base64 = buffer.toString('base64');
+            const mime = image.type;
+            imageUpload = `data:${mime};base64,${base64}`;
+        }
+
+        const title = formData.get('title') || '';
+        const content = formData.get('content') || '';
+        const address = event.params.address;
+
+        if (!content || content.length < 3 || content.length > 5000) {
+            return fail(400, { message: 'Content must be between 3 and 5000 characters' });
+        }
+
+        const table = newsgroupAddress(address);
+        await db.insert(table).values({
+            id: crypto.randomUUID(),
+            type: "post",
+            title,
+            content,
+            image: imageUpload,
+            username: event.locals.user.username,
+            createdBy: event.locals.user.id,
+            sentByGuest: event.locals.user.isGuest,
+        });
+
+        return { status: 200, body: { message: 'Post created successfully' } };
+    }
+}
--- src/routes/[address]/news/+page.svelte
+++ src/routes/[address]/news/+page.svelte
@@ -2,20 +2,11 @@
     import Dialog from "$lib/Dialog.svelte";
     import { slide } from "svelte/transition";
     import {boolean, text, timestamp, varchar} from "drizzle-orm/pg-core";
+    import {onMount} from "svelte";
 
     let { data } = $props();
 
-    let drafts = $state([
-        {
-            id: "draft_" + crypto.randomUUID(),
-            type: "post",
-            title: "New Draft",
-            content: "This is a new draft. Edit me!",
-            username: data.user.username,
-            authorId: data.user.id,
-            sentByGuest: data.user.isGuest,
-        }
-    ]);
+    let drafts = $state([]);
     let currentlySelected = $state(null);
 
     const createDraft = () => {
@@ -31,9 +22,19 @@
         drafts.push(newDraft);
         currentlySelected = newDraft.id;
     }
+
+    function exitIfDraft(event) {
+        if (drafts.length > 0) event.preventDefault();
+        event.returnValue = drafts.length > 0 ? "true" : null;
+        return drafts.length > 0 ? "true" : null;
+    }
 </script>
 
-<div>
+<svelte:window on:beforeunload={exitIfDraft}></svelte:window>
+
+{@debug drafts}
+
+<div >
     <div class="absolute inset-0 -z-50 bg-[#242424]">
         <div class="absolute inset-0 bg-[url('/background/wave8.svg')] bg-cover bg-center wave-animation [animation-duration:6s] -z-42"></div>
         <div class="absolute inset-0 bg-[url('/background/wave7.svg')] bg-cover bg-center wave-animation [animation-duration:9s] -z-43"></div>
@@ -53,11 +54,11 @@
                 <span>{data.user.username}</span>
             </div>
         </div>
-        <div class="w-11/12 mx-auto rounded-t-[75px] min-h-[calc(100vh-80px)] bg-neutral-500/10 backdrop-blur-md" transition:slide>
+        <div class="w-11/12 mx-auto rounded-t-[75px] min-h-[calc(100vh-80px)] h-[calc(100vh-80px)] bg-neutral-500/10 backdrop-blur-md" transition:slide>
             {#await data.data}
                 loading...
             {:then posts}
-                <div class="pt-5 flex flex-col">
+                <div class="pt-5 flex flex-col h-full">
                     <div class="pt-1 pb-2 mx-[200px] flex flex-row justify-between">
                         <div>
                             {posts.length} items
@@ -69,23 +70,37 @@
                     </div>
                     <hr class="w-full mx-auto">
                     <!-- actual menus -->
-                    <div class="mx-2 flex flex-row inset-0">
+                    <div class="mx-2 flex flex-row inset-0 h-full">
                         <!-- list -->
-                        <div class="min-h-[calc(100vh-170px)] border-r-2 border-neutral-500 inset-0 w-1/2 pr-2">
+                        <div class="h-full border-r-2 border-neutral-500 inset-0 w-1/2 pr-2">
                             {#if posts.length > 0 || drafts.length > 0}
                                 {#each drafts as draft (draft.id)}
-                                    <div class="flex flex-row items-center space-x-2 hover:bg-neutral-500/20 p-2 rounded-md cursor-pointer"
+                                    <button class="flex flex-row items-center space-x-2 hover:bg-neutral-500/20 p-2 rounded-md cursor-pointer w-full"
                                          onclick={() => {
                                              currentlySelected = draft.id;
                                          }}>
-                                        <div class="w-10 h-10 bg-neutral-500/20 rounded-full flex items-center justify-center">
+                                        <span class="w-10 h-10 bg-neutral-500/20 rounded-full flex shrink-0 items-center justify-center">
                                             <span class="text-lg font-bold">{draft.title[0]}</span>
-                                        </div>
-                                        <div class="flex flex-col">
+                                        </span>
+                                        <span class="flex flex-col items-start grow-0">
                                             <span class="font-bold">{draft.title}</span>
-                                            <span class="text-sm text-neutral-400">{draft.content.slice(0, 50)}...</span>
-                                        </div>
-                                    </div>
+                                            <span class="text-sm text-neutral-400 overflow-ellipsis line-clamp-1 ">{draft.content.slice(0, 75)}...</span>
+                                        </span>
+                                    </button>
+                                {/each}
+                                {#each posts as post (post.id)}
+                                    <button class="flex flex-row items-center space-x-2 hover:bg-neutral-500/20 p-2 rounded-md cursor-pointer w-full"
+                                         onclick={() => {
+                                             currentlySelected = post.id;
+                                         }}>
+                                        <span class="w-10 h-10 bg-neutral-500/20 rounded-full flex shrink-0 items-center justify-center">
+                                            <span class="text-lg font-bold">{post.title[0]}</span>
+                                        </span>
+                                        <span class="flex flex-col items-start grow-0">
+                                            <span class="font-bold">{post.title}</span>
+                                            <span class="text-sm text-neutral-400 overflow-ellipsis line-clamp-1 ">{post.content.slice(0, 75)}...</span>
+                                        </span>
+                                    </button>
                                 {/each}
                             {:else}
                                 <div class="flex flex-row items-center space-x-5">
@@ -130,11 +145,12 @@
                             {/if}
                         </div>
                         <!-- reader -->
-                        <div class="w-1/2 pl-2">
+                        <div class="h-full w-1/2 pl-2">
                             {#if currentlySelected}
+                                <!-- draft editor form -->
                                 {#if currentlySelected.startsWith('draft')}
                                     {@const draft = drafts.find(d => d.id === currentlySelected)}
-                                    <div class="flex flex-col">
+                                    <form method="POST" action="?/createPost" enctype="multipart/form-data" class="h-full flex flex-col">
                                         <div class="w-full px-2 py-2 border-b-2 border-neutral-500">
                                             To: <b>{data.address.address}</b>
                                         </div>
@@ -144,19 +160,51 @@
                                                 id="title"
                                                 name="title"
                                                 type="text"
+                                                required
                                                 placeholder="subject"
                                                 bind:value={draft.title}
-                                                class="border-b-1 border-neutral-500 w-full"
+                                                class="border-b-1 border-neutral-500 w-full field-sizing-[normal]"
                                             >
+                                        </div>
+                                        <div class="flex flex-row space-x-2 w-full px-2 py-2 border-b-2 border-neutral-500">
+                                            <label for="title">Image (optional): </label>
+                                            <input type="file" name="image" accept="image/*" class="file:bg-neutral-500 file:cursor-pointer file:px-1" />
                                         </div>
                                         <div class="flex flex-col w-full h-full px-2 py-2">
                                             <textarea
                                                 id="content"
+                                                required
                                                 name="content"
                                                 placeholder="write your post here..."
                                                 bind:value={draft.content}
-                                                class="border-b-1 border-neutral-500 w-full h-64 resize-y"
+                                                class="h-full border-b-1 border-neutral-500 w-full resize-none"
                                             ></textarea>
+                                        </div>
+                                        <div class="flex flex-row">
+                                            <button onclick={() => { drafts = drafts.filter(v => v.id !== draft.id); currentlySelected = null; }} class="mb-2 p-2 cursor-pointer hover:bg-neutral-500/50 active:bg-neutral-600/50 shrink">delete</button>
+                                            <button class="mb-2 p-2 cursor-pointer border-2 border-neutral-500 hover:bg-neutral-500/50 active:bg-neutral-600/50 grow">submit</button>
+                                        </div>
+                                    </form>
+                                {:else}
+                                <!-- post reader crap -->
+                                    {@const post = posts.find(p => p.id === currentlySelected)}
+                                    <div class="h-full flex flex-col">
+                                        <div class="w-full px-2 py-2 border-b-2 border-neutral-500">
+                                            To: <b>{data.address.address}</b>
+                                        </div>
+                                        <div class="flex flex-row space-x-2 w-full px-2 py-2 border-b-2 border-neutral-500">
+                                            <span class="font-bold">{post.title}</span>
+                                            <span class="text-sm text-neutral-400">by {post.username} on {post.createdAt.toLocaleDateString()}</span>
+                                        </div>
+                                        <div class="flex flex-col w-full h-full px-2 py-2">
+                                            <p class="whitespace pre-wrap break-words">{post.content}</p>
+                                            {#if post.image}
+                                                <img src={post.image} alt="Post image" class="max-w-full max-h-96 object-contain mt-2" />
+                                            {/if}
+                                        </div>
+                                        <div class="flex flex-row">
+                                            <button onclick={() => { currentlySelected = null; }} class="mb-2 p-2 cursor-pointer hover:bg-neutral-500/50 active:bg-neutral-600/50 shrink">back</button>
+                                            <button class="mb-2 p-2 cursor-pointer border-2 border-neutral-500 hover:bg-neutral-500/50 active:bg-neutral-600/50 grow">reply</button>
                                         </div>
                                     </div>
                                 {/if}
