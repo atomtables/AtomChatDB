73c1873476c308f2f5814099f453a04845d5ccd4
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-07-19 16:19:25 +0530

    add action to allow creating proposals

--- src/routes/(receive)/(connect)/[address]/+page.server.js
+++ src/routes/(receive)/(connect)/[address]/+page.server.js
@@ -1,8 +1,26 @@
-import {db} from "$lib/server/db/index.js";
+import {db, groupaddr} from "$lib/server/db/index.js";
 import * as table from "$lib/server/db/schema.ts";
 import {eq} from "drizzle-orm";
 import {fail, redirect} from "@sveltejs/kit";
 import {invalidateGuestSession, invalidateSession, validateSessionToken} from "$lib/server/auth.js";
 
 export const actions = {
+    createProposal: async (event) => {
+        if (!event.locals.session) return fail(401, { message: 'Unauthorized' });
+        if (!event.locals.user) return fail(401, { message: 'Unauthorized' });
+
+        const formData = await event.request.formData();
+        const description = formData.get('description');
+        const whyShould = formData.get('whyShould');
+        const type = formData.get('type');
+
+        const sendFormData = new FormData();
+        sendFormData.append("title", event.params.address)
+        sendFormData.append('content',  `+${type}=\n++Description+ ${description} end==\n+++Reason+ ${whyShould} end===`);
+
+        await event.fetch(`/news.groups.proposals/news/post`, {
+            method: 'POST',
+            body: sendFormData,
+        });
+    }
 }
