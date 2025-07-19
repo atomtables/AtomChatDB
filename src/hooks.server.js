3e678babbfe883429012761512a60ccf39457446
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-07-19 16:25:51 +0530

    improve the init stuff to form database tables

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,6 @@
 import * as auth from '$lib/server/auth';
 import * as fs from "node:fs";
+import {db} from "$lib/server/db/index.js";
 
 
 export const handle = async ({ event, resolve }) => {
@@ -49,7 +50,12 @@ export const handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
-export const init = () => {
+export const init = async () => {
 	fs.mkdirSync("static/public/dp", { recursive: true });
 	fs.mkdirSync("static/public/images", { recursive: true });
+
+	try { await db.execute(`insert into groups values ('news.groups.proposals', 'internalsystem', '1970-01-01', 'The place to insert new proposals to create new groups. It is recommended that you use the "request creation" dialogue to submit a request. You can view and reply to other proposals, seeing which ones were rejected and accepted.', 'news') on conflict do nothing`); } catch (e) { console.log("existing group:", e.message) }
+	try { await db.execute(`CREATE TABLE IF NOT EXISTS "address_news.groups.proposals" (LIKE "group_template" INCLUDING ALL)`) } catch (e) { console.log(e.message); }
+	try { await db.execute(`ALTER TABLE "address_news.groups.proposals" ADD CONSTRAINT "news.groups.proposals_replyTo_news.groups.proposals_id_fk" FOREIGN KEY ("replyTo") REFERENCES "public"."address_news.groups.proposals"("id") ON DELETE no action ON UPDATE no action`) } catch (e) { console.log(e.message); }
+	try { await db.execute(`insert into users values ('internalsystem', '1970-01-01', 'computer', 'AtomChatDB Internal System', '', false)`) } catch (e) { console.log("existing user:", e.message); }
 }
